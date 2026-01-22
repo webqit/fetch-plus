6b2114b5134df90623091dfae0129baf1fa9500b
Ox-Harris👋 <oxharris.dev@gmail.com>
2026-01-22 12:05:47 +0100

    Bump URLPlus version to 0.1.5

--- src/HeadersPlus.js
+++ src/HeadersPlus.js
@@ -176,8 +176,11 @@ export function renderCookieObjToString(cookieObj) {
     const attrsArr = [`${cookieObj.name}=${/*encodeURIComponent*/(cookieObj.value)}`];
     for (const attrName in cookieObj) {
         if (['name', 'value'].includes(attrName)) continue;
+
         let _attrName = attrName[0].toUpperCase() + attrName.substring(1);
         if (_attrName === 'MaxAge') { _attrName = 'Max-Age' };
+
+        if (cookieObj[attrName] === false) continue;
         attrsArr.push(cookieObj[attrName] === true ? _attrName : `${_attrName}=${cookieObj[attrName]}`);
     }
     return attrsArr.join('; ');
